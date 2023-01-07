acd05ff71239e634e0f3a312d3d31057a0dc4627
yusuf <yusuf@DESKTOP-4BCDGTC>
2023-01-07 17:53:43 +0300

    Add project files.

---
+++ palindrometry/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrometry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            Console.WriteLine("Enter a String");
+            str = Console.ReadLine();
+
+            char[] arr = str.ToCharArray();
+            Array.Reverse(arr);
+
+            string str_reverse = new string(arr);
+
+            if(str.ToLower().Equals(str_reverse.ToLower())) 
+            {
+                Console.WriteLine(str + " ->Its A palindrome string");
+            }
+            else
+            {
+                Console.WriteLine(str + " ->Its Not a palindrome string");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
